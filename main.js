8584aebc8af07ecb38b9caecb86642d16aa34325
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-22 13:55:32 -0400

    When d6 is clicked, image for dice matches the number rolled

--- main.js
+++ main.js
@@ -9,59 +9,60 @@ const twenties = [];
 
 /********************
  * HELPER FUNCTIONS *
-********************/
+ ********************/
 
-const getRandomNumber = function(max) {
-    const rand = Math.random();
-    const range = rand * max;
-    const result = Math.ceil(range);
-    
-    return result;
-}
+const getRandomNumber = function (max) {
+	const rand = Math.random();
+	const range = rand * max;
+	const result = Math.ceil(range);
 
-const sortByNumber = function(arr) {
-  const byNumber = function(item1, item2) {
-    return item1 - item2;
-  }
+	return result;
+};
 
-  return arr.slice().sort(byNumber);
-}
+const sortByNumber = function (arr) {
+	const byNumber = function (item1, item2) {
+		return item1 - item2;
+	};
+
+	return arr.slice().sort(byNumber);
+};
 
 /*******************
  * YOUR CODE BELOW *
  *******************/
 
-
-
 /*******************
  * EVENT LISTENERS *
  *******************/
-const d6Roll = document.getElementById('d6-roll');
-d6Roll.addEventListener('click', function() {
-  sixes.push(getRandomNumber(6));
-  console.log(sortByNumber(sixes))
-}
-);
+const d6Roll = document.getElementById("d6-roll");
+d6Roll.addEventListener("click", function () {
+  rolledD6 = getRandomNumber(6)
+	sixes.push(rolledD6);
+	console.log("d6:", sortByNumber(sixes));
+  d6Roll.src = `images/d6/${rolledD6}.png`
+});
 
 /******************
  * RESET FUNCTION *
  ******************/
- const resetAllRolls = document.getElementById('reset-button');
- resetAllRolls.addEventListener('click', function() {
-   console.log("reset button clicked")
-   sixes.splice(0, sixes.length)
-   console.log(sixes)
-   //reset all arrays to empty
-   //reset images to original
- }
- );
-
+const resetAllRolls = document.getElementById("reset-button");
+resetAllRolls.addEventListener("click", function () {
+	console.log("reset button clicked");
+	sixes.splice(0, sixes.length);
+  console.log("d6:", sixes);
+	doubleSixes.splice(0, doubleSixes.length);
+  console.log("2d6:", doubleSixes);
+	twelves.splice(0, twelves.length);
+  console.log("d12:", twelves);
+	twenties.splice(0, twenties.length);
+	console.log("d20:", twenties);
+	//reset all arrays to empty
+	//reset images to original
+});
 
 /****************************
  * CLICK HANDLING FUNCTIONS *
-****************************/
-
-
+ ****************************/
 
 /****************
  * MATH SECTION *
